142b784235c5bdc5d06f0c74691d3c04452902cf
Oscar Salazar <oscarsalazarsevilla@outlook.com>
2019-01-08 16:55:15 -0600

    Se completo el Backend y el ApiServices con el CRUD de Productos. Ambas Apps se Publicaron el Azure. Backend: https://salesbackend.azurewebsites.net/ ApiServices: https://salesapisevices.azurewebsites.net/

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Common.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublshOn { get; set; }
+    }
+}
