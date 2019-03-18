d43ef6ceaa66453a14141121109b8ab0cac7d3dd
Oscar Salazar <oscarsalazarsevilla@outlook.com>
2019-03-18 04:10:05 -0600

    Se culmino el Video 49, se customizaron los Splash's tanto para Android como para iSO y los iconos de la App

--- Sales/Sales.Android/MainActivity.cs
+++ Sales/Sales.Android/MainActivity.cs
@@ -1,14 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
+using Android.Runtime;
 using ImageCircle.Forms.Plugin.Droid;
-using Plugin.Permissions;
 using Plugin.CurrentActivity;
+using Plugin.Permissions;
 
 namespace Sales.Droid
 {
-    [Activity(Label = "Sales", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Sales", 
+        Icon = "@drawable/ic_launcher", 
+        Theme = "@style/MainTheme",
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
